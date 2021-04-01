73e958a2968a19d65f3f9117c368b2fa2ad7082e
Keginold Philip <keginoldp@hotmail.com>
2021-04-01 15:12:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(attributes)
-  attributes.each {|key, value| self.send (("#{key}=learn "), value)}
+  attributes.each {|key, value| self.send (("#{key}"), value)}
 end
 end
